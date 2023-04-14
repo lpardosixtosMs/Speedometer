bda1b2f0eb24b8d6975b7c9fd6027e333b7e0250
issack john <issackjohn@microsoft.com>
2023-04-14 15:10:15 -0700

    Formatting embedded todo mvc properly

--- resources/todomvc/architecture-examples/react/src/index-big-0.js
+++ resources/todomvc/architecture-examples/react/src/index-big-0.js
@@ -7,12 +7,19 @@ import "todomvc-app-css/index.css";
 import "./small.css";
 
 render(
-    <section className="todoapp" id="root">
-        <HashRouter>
-            <Routes>
-                <Route path="*" element={<App />} />
-            </Routes>
-        </HashRouter>
-    </section>,
+    <div class="absolutely-positioned-element">
+        <section className="todoapp" id="root">
+            <HashRouter>
+                <Routes>
+                    <Route path="*" element={<App />} />
+                </Routes>
+            </HashRouter>
+        </section><footer class="info">
+            <p>Click on input field to write your todo.</p>
+            <p>At least two characters are needed to be a valid entry.</p>
+            <p>Press 'enter' to add the todo.</p>
+            <p>Double-click to edit a todo</p>
+        </footer>
+    </div>,
     document.getElementById("gen-0-0")
 );
