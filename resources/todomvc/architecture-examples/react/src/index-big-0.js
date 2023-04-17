70cfeea916e5708d52884af6b84405679bcffe30
Issack John <issackjohn@microsoft.com>
2023-04-17 15:24:16 -0700

    add Recreated static page for speedometer

--- resources/todomvc/architecture-examples/react/src/index-big-0.js
+++ resources/todomvc/architecture-examples/react/src/index-big-0.js
@@ -5,9 +5,13 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 import { App } from "./todo/app";
 import "todomvc-app-css/index.css";
 import "./small.css";
+import "./mail.css";
+
+const todoHolder = document.createElement("div");
+todoHolder.className = "absolutely-positioned-element";
 
 render(
-    <div class="absolutely-positioned-element">
+    <>
         <section className="todoapp" id="root">
             <HashRouter>
                 <Routes>
@@ -21,6 +25,8 @@ render(
             <p>Press 'enter' to add the todo.</p>
             <p>Double-click to edit a todo</p>
         </footer>
-    </div>,
-    document.getElementById("gen-0-0")
+    </>,
+    todoHolder
 );
+
+document.getElementById("root-ish").appendChild(todoHolder);
