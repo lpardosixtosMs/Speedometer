c709866f94d8402e5581db651dbce267cb0333d3
Luis Fernando Pardo Sixtos <lpardosixtos@microsoft.com>
2023-05-02 12:53:15 -0700

    Add spectrum-matching test

--- resources/tests.mjs
+++ resources/tests.mjs
@@ -438,6 +438,36 @@ Suites.push({
 });
 
 Suites.push({
+    name: "TodoMVC-React-Spectrum-Matching",
+    url: "todomvc/architecture-examples/react/dist/spectrum-matching/index.html",
+    async prepare(page) {
+        const element = await page.waitForElement(".new-todo");
+        element.focus();
+        page._frame.contentWindow.todo = page.getElementById('root');
+    },
+    tests: [
+        new BenchmarkTestStep(`Adding${numberOfItemsToAdd}Items`, (page) => {
+            const newTodo = page._frame.contentWindow.todo.querySelector(".new-todo");
+            for (let i = 0; i < numberOfItemsToAdd; i++) {
+                newTodo.setValue(`Something to do ${i}`);
+                newTodo.dispatchEvent("input");
+                newTodo.enter("keydown");
+            }
+        }),
+        new BenchmarkTestStep("CompletingAllItems", (page) => {
+            const checkboxes = page._frame.contentWindow.todo.querySelectorAll(".toggle");
+            for (let i = 0; i < numberOfItemsToAdd; i++)
+                checkboxes[i].click();
+        }),
+        new BenchmarkTestStep("DeletingAllItems", (page) => {
+            const deleteButtons = page._frame.contentWindow.todo.querySelectorAll(".destroy");
+            for (let i = 0; i < numberOfItemsToAdd; i++)
+                deleteButtons[i].click();
+        }),
+    ],
+});
+
+Suites.push({
     name: "TodoMVC-React-Redux",
     url: "todomvc/architecture-examples/react-redux/dist/index.html",
     async prepare(page) {
