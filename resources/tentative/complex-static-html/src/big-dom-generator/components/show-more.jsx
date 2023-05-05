093cac4615bb904b873e16ba637ec442607b9ec2
Luis Fernando Pardo Sixtos <lpardosixtos@microsoft.com>
2023-05-05 15:10:56 -0700

    Added A popover menu for the show-more button

--- resources/tentative/complex-static-html/src/big-dom-generator/components/show-more.jsx
+++ resources/tentative/complex-static-html/src/big-dom-generator/components/show-more.jsx
@@ -2,10 +2,31 @@ import { ActionButton } from "./action-group";
 
 import MoreIcon from "./../assets/Smock_MoreCircle_18_N.svg";
 
+const PopOver = () => {
+    const numOptions = 10;
+    const children = [];
+    for (let i = 0; i < numOptions; i++) {
+        children.push((
+            <li key={i} className="ui spectrum-Menu-item" role="menuitem" tabindex="0">
+                <span className="ui spectrum-Menu-itemLabel">Hidden Option {i}</span>
+            </li>
+        ));
+    }
+
+    return (
+        <div className="ui spectrum-Popover spectrum-Popover--bottom">
+            <ul class="ui spectrum-Menu" role="menu">
+                {children}
+            </ul>
+        </div>
+    );
+}
+
 export const ShowMore = () => {
     return (
         <div className="ui show-more">
             <ActionButton Icon={MoreIcon} quiet />
+            <PopOver />
         </div>
     );
 };
