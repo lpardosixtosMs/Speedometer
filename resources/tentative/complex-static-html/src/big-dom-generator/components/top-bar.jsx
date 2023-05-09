76cd3fd4c9cb280d1c74fce352875912719a7ef4
Issack John <issackjohn@microsoft.com>
2023-05-09 16:51:57 -0700

    done clean up

--- resources/tentative/complex-static-html/src/big-dom-generator/components/top-bar.jsx
+++ resources/tentative/complex-static-html/src/big-dom-generator/components/top-bar.jsx
@@ -14,7 +14,7 @@ const ContextualHelp = () => {
         <>
             <ActionButton Icon={HelpIcon} quite={false} />
             <PopOver role="presentation" className="ui spectrum-Popover spectrum-Popover--sizeM spectrum-Popover--bottom-start spectrum-ContextualHelp-popover">
-                <div className="ui" style={{ top: 25 }}>
+                <div className="ui">
                     <h2 className="ui spectrum-ContextualHelp-heading">Permission required</h2>
                     <p className="ui spectrum-ContextualHelp-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 </div>
@@ -25,7 +25,7 @@ const ContextualHelp = () => {
 
 const ProfileCard = () => {
     return (
-        <div className="ui spectrum-Card spectrum-Card--sizeS" tabindex="0" role="figure">
+        <div className="ui spectrum-Card spectrum-Card--sizeS" tabIndex="0" role="figure">
             <div className="ui spectrum-Card-coverPhoto"></div>
             <div className="ui spectrum-Card-body">
                 <div className="ui spectrum-Card-header">
@@ -47,7 +47,7 @@ export const TopBar = () => {
     const NUM_OF_SETTINGS = 5;
     return (
         <div className="ui top-bar">
-            <img className="ui" src={SpeedometerLogo} alt="Speedometer Logo" height={40} />
+            <img className="ui" src={SpeedometerLogo} alt="Speedometer Logo for TODO App" height={40} />
             <h2 className="ui spectrum-Heading spectrum-Heading--sizeL">TODO App</h2>
             <div className={`ui search-area`}>
                 <SearchArea />
@@ -56,11 +56,11 @@ export const TopBar = () => {
                 <ActionGroup>
                     <ContextualHelp />
                     <ActionButton Icon={BellIcon} quite={false} />
-                    <PopOver numOptions={NUM_OF_NOTIFICATIONS} className="ui spectrum-Popover spectrum-Popover--bottom" />
+                    <PopOver numOptions={NUM_OF_NOTIFICATIONS} className="ui spectrum-Popover spectrum-Popover--bottom" role="dialog" />
                     <ActionButton Icon={SettingsIcon} quite={false} />
-                    <PopOver numOptions={NUM_OF_SETTINGS} className="ui spectrum-Popover spectrum-Popover--bottom" />
+                    <PopOver numOptions={NUM_OF_SETTINGS} className="ui spectrum-Popover spectrum-Popover--bottom" role="dialog" />
                     <ActionButton Icon={ProfileIcon} quite={false} />
-                    <PopOver className="ui spectrum-Popover spectrum-Popover--bottom-start">
+                    <PopOver className="ui spectrum-Popover spectrum-Popover--bottom-start" role="dialog">
                         <ProfileCard />
                     </PopOver>
                 </ActionGroup>
