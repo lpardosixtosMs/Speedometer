2fa2fe9755d4e5a1b0466cd3529575687a698a27
issack john <issackjohn@microsoft.com>
2023-05-23 13:06:53 -0700

    embedded preact

---
+++ resources/todomvc/architecture-examples/preact/src/embedded.js
@@ -0,0 +1,29 @@
+import { h, createElement, render, Fragment } from "preact";
+import App from "./app/app";
+
+import "todomvc-app-css/index.css";
+import "./styles.css";
+import "../../../big-dom-generator/public/layout.css";
+import "../../../big-dom-generator/generated.css";
+import "../../../big-dom-generator/dist/app.css";
+
+const todoHolder = document.createElement("div");
+todoHolder.className = "todoholder";
+
+render(
+    <Fragment>
+        <section className="todoapp" id="root">
+            <App />
+        </section>
+        <footer className="info">
+            <p>Click on input field to write your todo.</p>
+            <p>At least two characters are needed to be a valid entry.</p>
+            <p>Press 'enter' to add the todo.</p>
+            <p>Double-click to edit a todo</p>
+        </footer>
+    </Fragment>,
+    todoHolder
+);
+
+const todoArea = document.querySelector(".todo-area");
+todoArea.appendChild(todoHolder);
