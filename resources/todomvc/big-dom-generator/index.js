a6d6a119e9a2b0227ac076fc86adfcf98831c6a7
issack john <issackjohn@microsoft.com>
2023-05-23 11:46:47 -0700

    add node modules to gitignore, fix build & serve

--- resources/todomvc/big-dom-generator/index.js
+++ resources/todomvc/big-dom-generator/index.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { renderToStaticMarkup } from "react-dom/server";
-import { App } from "./app";
+import { App } from "./src/app";
 import { genCss } from "./gen-css";
 
-import "./app.css";
+import "./src/app.css";
 
 const fs = require("fs");
 
 const randomCss = genCss();
-fs.writeFileSync("src/react-todomvc/public/generated.css", randomCss);
+fs.writeFileSync("./generated.css", randomCss);
 
 const html = `<!DOCTYPE html>
-<html lang="en" class="spectrum spectrum--medium spectrum--light">
+<html lang="en" class="ui spectrum spectrum--medium spectrum--light">
   <head>
     <title>Big Todo App</title>
+    <link rel="stylesheet" href="app.css">
   </head>
-  <body>
+  <body class="ui">
     ${renderToStaticMarkup(<App />)}
   </body>
 </html>`;
-fs.writeFileSync("generator-dist/index.html", html);
+fs.writeFileSync("dist/index.html", html);
