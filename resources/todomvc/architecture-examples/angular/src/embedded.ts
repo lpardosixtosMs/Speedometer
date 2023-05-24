2ac2c6b856a34f700f125ec21d426a304658b95d
issack john <issackjohn@microsoft.com>
2023-05-24 12:36:58 -0700

    embedded angular

---
+++ resources/todomvc/architecture-examples/angular/src/embedded.ts
@@ -0,0 +1,12 @@
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { AppModule } from "./app/app.module";
+
+const divElement = document.createElement("div");
+divElement.className = "todoholder";
+const todoArea = document.querySelector(".todo-area");
+if (todoArea)
+    todoArea.appendChild(divElement);
+
+platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch((err) => console.error(err));
