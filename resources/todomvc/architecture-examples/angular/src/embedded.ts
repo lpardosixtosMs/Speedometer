f2a99781b268060631f2426d833344fac50dbffe
issack john <issackjohn@microsoft.com>
2023-05-30 15:00:37 -0700

    update angular embedded dist

--- resources/todomvc/architecture-examples/angular/src/embedded.ts
+++ resources/todomvc/architecture-examples/angular/src/embedded.ts
@@ -4,7 +4,8 @@ import { AppModule } from "./app/app.module";
 
 import "../../../big-dom-generator/dist/app.css";
 import "../../../big-dom-generator/public/layout.css";
-import "../../../big-dom-generator/generated.css";
+import "../../../big-dom-generator/matchingCss.css";
+import "../../../big-dom-generator/nonMatchingCss.css";
 
 const divElement = document.createElement("div");
 divElement.className = "todoholder";
