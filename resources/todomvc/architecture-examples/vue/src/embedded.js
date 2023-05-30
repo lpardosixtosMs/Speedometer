0c9cbbcd6c9e2868c13a5cdad7c6dfdec9af4045
issack john <issackjohn@microsoft.com>
2023-05-30 14:40:38 -0700

    update vue embedded dist

--- resources/todomvc/architecture-examples/vue/src/embedded.js
+++ resources/todomvc/architecture-examples/vue/src/embedded.js
@@ -3,7 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import "../../../big-dom-generator/dist/app.css";
 import "../../../big-dom-generator/public/layout.css";
-import "../../../big-dom-generator/generated.css";
+import "../../../big-dom-generator/matchingCss.css";
+import "../../../big-dom-generator/nonMatchingCss.css";
 
 const app = createApp(App);
 
