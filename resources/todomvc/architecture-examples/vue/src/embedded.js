0cdc7325e515d68e63473ddf2c7434dacae8a0de
issack john <issackjohn@microsoft.com>
2023-05-29 17:01:41 -0700

    fix vue embedded complex dom

--- resources/todomvc/architecture-examples/vue/src/embedded.js
+++ resources/todomvc/architecture-examples/vue/src/embedded.js
@@ -6,8 +6,12 @@ import "../../../big-dom-generator/dist/app.css";
 import "../../../big-dom-generator/generated.css";
 
 const app = createApp(App);
-console.log("embedded.js: app created")
+
+const todoHolder = document.createElement("div");
+todoHolder.className = "todoholder";
 
 app.use(router);
 
-app.mount(".todo-area");
+app.mount(todoHolder);
+
+document.querySelector(".todo-area").appendChild(todoHolder);
