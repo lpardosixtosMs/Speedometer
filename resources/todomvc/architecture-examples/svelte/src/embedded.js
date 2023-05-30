26e2abd9fb6073ac899066b554bef23960b6f7b0
issack john <issackjohn@microsoft.com>
2023-05-30 14:48:01 -0700

    update svelte embedded dist

--- resources/todomvc/architecture-examples/svelte/src/embedded.js
+++ resources/todomvc/architecture-examples/svelte/src/embedded.js
@@ -1,7 +1,8 @@
 import App from "./App.svelte";
 import "../../../big-dom-generator/dist/app.css";
 import "../../../big-dom-generator/public/layout.css";
-import "../../../big-dom-generator/generated.css";
+import "../../../big-dom-generator/matchingCss.css";
+import "../../../big-dom-generator/nonMatchingCss.css";
 
 const app = new App({
     target: document.querySelector(".todoapp"),
