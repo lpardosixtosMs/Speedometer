be77fac6c387d3ce6eb1fa56dc73b244c2342c59
issack john <issackjohn@microsoft.com>
2023-06-21 00:03:17 -0700

    building to correct directory

--- resources/todomvc/architecture-examples/vue/standalone/vue.config.js
+++ resources/todomvc/architecture-examples/vue/standalone/vue.config.js
@@ -1,11 +1,19 @@
 const { defineConfig } = require("@vue/cli-service");
+const { getHtmlContent } = require("../shared/utils/getHtmlContent");
+
 module.exports = defineConfig({
     transpileDependencies: true,
     publicPath: "",
+    outputDir: "standalone/dist", // Change the output directory to "dist/my-app"
     chainWebpack: (config) => {
         config.plugin("html").tap((args) => {
-          args[0].template = "shared/public/index.html";
-          return args;
+            args[0].title = "TodoMVC: Vue";
+            args[0].template = "shared/public/index.html";
+            args[0].templateParameters = {
+                body: getHtmlContent("standalone/public/partial.html"),
+                htmlClasses: "",
+            };
+            return args;
         });
         config.entry("app").clear().add("./standalone/src/main.js");
     },
