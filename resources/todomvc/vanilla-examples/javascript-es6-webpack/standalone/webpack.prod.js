46e3cefa40228757753b5bfde20924592ad73dba
issack john <issackjohn@microsoft.com>
2023-06-28 10:21:31 -0700

    fix es6-webpack

--- resources/todomvc/vanilla-examples/javascript-es6-webpack/standalone/webpack.prod.js
+++ resources/todomvc/vanilla-examples/javascript-es6-webpack/standalone/webpack.prod.js
@@ -6,7 +6,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
-const { getHtmlContent } = require("../shared/utils/getHtmlContent.js");
+const { getHtmlContent } = require("big-dom-generator/utils/getHtmlContent.js");
 
 module.exports = merge(common, {
     mode: "production",
