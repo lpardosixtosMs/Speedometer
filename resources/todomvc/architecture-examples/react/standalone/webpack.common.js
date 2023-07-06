f2b409973c3d72efd8b90460a7d0b36d9df16710
issack john <issackjohn@microsoft.com>
2023-07-05 17:33:23 -0700

    restructure webpack standalone configs

--- resources/todomvc/architecture-examples/react/standalone/webpack.common.js
+++ resources/todomvc/architecture-examples/react/standalone/webpack.common.js
@@ -1,7 +1,21 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const { getHtmlContent } = require("big-dom-generator/utils/getHtmlContent");
+
 module.exports = {
     entry: {
         app: "./standalone/src/index.js",
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "TodoMVC: React",
+            template: "shared/public/index.html",
+            templateParameters: {
+                body: getHtmlContent("shared/public/partial.html"),
+                htmlClasses: "",
+            },
+        }),
+    ],
     resolve: {
         extensions: [".js", ".jsx"],
     },
