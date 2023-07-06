eb27872c572b5166f1361d9d2d8e8395153b0ca1
issack john <issackjohn@microsoft.com>
2023-07-06 15:27:03 -0700

    restore react-redux structure.

--- resources/todomvc/architecture-examples/react-redux/webpack.common.js
+++ resources/todomvc/architecture-examples/react-redux/webpack.common.js
@@ -1,20 +1,14 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const { getHtmlContent } = require("big-dom-generator/utils/getHtmlContent");
-
 module.exports = {
     entry: {
-        app: "./standalone/src/index.js",
+        app: "./src/index.js",
     },
     plugins: [
         new HtmlWebpackPlugin({
             title: "TodoMVC: React-Redux",
-            template: "shared/public/index.html",
-            templateParameters: {
-                body: getHtmlContent("shared/public/partial.html"),
-                htmlClasses: "",
-            },
+            template: "public/index.html",
         }),
     ],
     output: {
