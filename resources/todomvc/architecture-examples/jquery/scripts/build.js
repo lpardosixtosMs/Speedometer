d96befee2bb994040bd64c81c7fa766cd20368f7
issack john <issackjohn@microsoft.com>
2023-07-06 16:51:17 -0700

    jquery done

--- resources/todomvc/architecture-examples/jquery/scripts/build.js
+++ resources/todomvc/architecture-examples/jquery/scripts/build.js
@@ -2,8 +2,8 @@ const fs = require("fs").promises;
 const path = require("path");
 
 const rootDirectory = "./";
-const sourceDirectory = "./shared/src";
-const targetDirectory = "./standalone/dist";
+const sourceDirectory = "./src";
+const targetDirectory = "./dist";
 
 const htmlFile = "index.html";
 
@@ -41,8 +41,7 @@ const build = async () => {
         html = html.replace(filesToMove[i], path.basename(filesToMove[i]));
 
     // remove basePath from source directory
-    const sourceDirectoryPathParts = sourceDirectory.split("/");
-    const basePath = `${sourceDirectoryPathParts[1]}/${sourceDirectoryPathParts[2]}/`;
+    const basePath = `${path.basename(sourceDirectory)}/`;
     const re = new RegExp(basePath, "g");
     html = html.replace(re, "");
 
