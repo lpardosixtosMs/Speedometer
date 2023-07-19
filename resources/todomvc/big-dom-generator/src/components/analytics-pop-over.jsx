79ef83649a0b605bd11bd805ca6b8c1ac5e12d12
issack john <issackjohn@microsoft.com>
2023-07-19 16:32:16 -0700

    add some styles to the notifications

--- resources/todomvc/big-dom-generator/src/components/analytics-pop-over.jsx
+++ resources/todomvc/big-dom-generator/src/components/analytics-pop-over.jsx
@@ -2,7 +2,7 @@ import { VerticalPopOver } from "./pop-over.jsx";
 
 const ProgressBar = ({ teamNumber, value }) => {
     return (
-        <div className="spectrum-ProgressBar spectrum-Meter--sizeS is-negative analytics-bar" value={value} role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100">
+        <div className={`spectrum-ProgressBar spectrum-Meter--sizeS is-negative analytics-bar team-${teamNumber}-progress`} value={value} role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100">
             <div className="spectrum-FieldLabel spectrum-FieldLabel--sizeS spectrum-ProgressBar-label">Team {teamNumber} progress</div>
             <div className="spectrum-FieldLabel spectrum-FieldLabel--sizeS spectrum-ProgressBar-percentage">{value}%</div>
             <div className="spectrum-ProgressBar-track">
