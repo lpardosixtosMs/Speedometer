e7c1f034c17ec5a0ff19e373467f10b7da7e803d
issack john <issackjohn@microsoft.com>
2023-08-10 12:35:25 -0700

    add extra css

--- resources/todomvc/vanilla-examples/javascript-web-components-complex/dist/add-extra-css.js
+++ resources/todomvc/vanilla-examples/javascript-web-components-complex/dist/add-extra-css.js
@@ -1,79 +1,112 @@
 const additionalStyleSheets = [];
 
 additionalStyleSheets[0] = new CSSStyleSheet();
-additionalStyleSheets[0].replaceSync(`
-:host([data-priority="0"]) {
-    --complex-border-bottom-color-completed: var(--complex-border-bottom-color-0, var(--complex-border-bottom-color-default));
-    --complex-border-bottom-color-not-completed: var(--complex-border-bottom-color-1, var(--complex-border-bottom-color-default));
-
-    --complex-background-color-completed: var(--complex-background-color-4, var(--complex-background-color-default));
-    --complex-background-color-not-completed: var(--complex-background-color-8, var(--complex-background-color-default));
-    
-    --complex-color-completed: var(--complex-color-1, var(--complex-color-default));
-    --complex-color-not-completed: var(--complex-color-0, var(--complex-color-default));
-
-    --complex-box-shadow: var(--complex-box-shadow-0, var(--complex-box-shadow-default));
+additionalStyleSheets[0].replaceSync(`li:not(.completed):not([data-priority="4"]) {
+    border-bottom-color: var(--complex-border-bottom-color-1);
 }
 
-:host([data-priority="1"]) {
-    --complex-border-bottom-color-completed: var(--complex-border-bottom-color-0, var(--complex-border-bottom-color-default));
-    --complex-border-bottom-color-not-completed: var(--complex-border-bottom-color-1, var(--complex-border-bottom-color-default));
-
-    --complex-background-color-completed: var(--complex-background-color-3, var(--complex-background-color-default));
-    --complex-background-color-not-completed: var(--complex-background-color-7, var(--complex-background-color-default));
-
-    --complex-color-completed: var(--complex-color-1, var(--complex-color-default));
-    --complex-color-not-completed: var(--complex-color-0, var(--complex-color-default));
-
-    --complex-box-shadow: var(--complex-box-shadow-0, var(--complex-box-shadow-default));
+li.completed:not([data-priority="4"]) {
+    border-bottom-color: var(--complex-border-bottom-color-0);
 }
 
-:host([data-priority="2"]) {
-    --complex-border-bottom-color-completed: var(--complex-border-bottom-color-0, var(--complex-border-bottom-color-default));
-    --complex-border-bottom-color-not-completed: var(--complex-border-bottom-color-1, var(--complex-border-bottom-color-default));
-
-    --complex-background-color-completed: var(--complex-background-color-2, var(--complex-background-color-default));
-    --complex-background-color-not-completed: var(--complex-background-color-6, var(--complex-background-color-default));
-
-    --complex-color-completed: var(--complex-color-1, var(--complex-color-default));
-    --complex-color-not-completed: var(--complex-color-0, var(--complex-color-default));
-
-    --complex-box-shadow: var(--complex-box-shadow-0, var(--complex-box-shadow-default));
+li:not(.completed):not([data-priority="4"]) > div > label {
+    color: var(--complex-color-0, var(--complex-color-default));
 }
 
-:host([data-priority="3"]) {
-    --complex-border-bottom-color-completed: var(--complex-border-bottom-color-0, var(--complex-border-bottom-color-default));
-    --complex-border-bottom-color-not-completed: var(--complex-border-bottom-color-1, var(--complex-border-bottom-color-default));
-
-    --complex-background-color-completed: var(--complex-background-color-1, var(--complex-background-color-default));
-    --complex-background-color-not-completed: var(--complex-background-color-5, var(--complex-background-color-default));
-
-    --complex-color-completed: var(--complex-color-1, var(--complex-color-default));
-    --complex-color-not-completed: var(--complex-color-0, var(--complex-color-default));
-
-    --complex-box-shadow: var(--complex-box-shadow-0, var(--complex-box-shadow-default));
-}
-
-li.not(.completed) {
-    border-bottom-color: var(--complex-border-bottom-color-not-completed);
-    background-color: var(--complex-background-color-not-completed);
-}
-
-li.completed {
-    background-color: var(--complex-background-color-completed);
-    border-bottom-color: var(--complex-border-bottom-color-completed);
-}
-
-li.not(.completed) > div > label {
-    color: var(--complex-color-not-completed);
-}
-
-lit.completed > div > label {
-    color: var(--complex-color-completed);
+li.completed:not([data-priority="4"]) > div > label {
+    color: var(--complex-color-1, var(--complex-color-default));
 }
 
 li.completed > div > :focus,
 li.completed > div > .toggle:focus + label {
-    box-shadow: var(--complex-box-shadow);}`);
+    box-shadow: var(--complex-box-shadow-0, var(--complex-box-shadow-default));}`);
+
+additionalStyleSheets[1] = new CSSStyleSheet();
+additionalStyleSheets[1].replaceSync(`li.completed {
+    background-color: var(--complex-background-color-1, var(--complex-background-color-default));
+}
+
+li:not(.completed):not([data-priority="4"]) {
+    border-bottom-color: var(--complex-border-bottom-color-1);
+}
+
+li.completed:not([data-priority="4"]) {
+    border-bottom-color: var(--complex-border-bottom-color-0);
+}
+
+li:not(.completed):not([data-priority="4"]) > div > label {
+    color: var(--complex-color-0, var(--complex-color-default));
+}
+
+li.completed:not([data-priority="4"]) > div > label {
+    color: var(--complex-color-1, var(--complex-color-default));
+}
+
+li.completed > div > :focus,
+li.completed > div > .toggle:focus + label {
+    box-shadow: var(--complex-box-shadow-0, var(--complex-box-shadow-default));
+}
+    
+li:not(.completed) {
+    background-color: var(--complex-background-color-5, var(--complex-background-color-default));}`);
+
+additionalStyleSheets[2] = new CSSStyleSheet();
+additionalStyleSheets[2].replaceSync(`li.completed {
+    background-color: var(--complex-background-color-2, var(--complex-background-color-default));
+
+}
+
+li:not(.completed):not([data-priority="4"]) {
+    border-bottom-color: var(--complex-border-bottom-color-1);
+}
+
+li.completed:not([data-priority="4"]) {
+    border-bottom-color: var(--complex-border-bottom-color-0);
+}
+
+li:not(.completed):not([data-priority="4"]) > div > label {
+    color: var(--complex-color-0, var(--complex-color-default));
+}
+
+li.completed:not([data-priority="4"]) > div > label {
+    color: var(--complex-color-1, var(--complex-color-default));
+}
+
+li.completed > div > :focus,
+li.completed > div > .toggle:focus + label {
+    box-shadow: var(--complex-box-shadow-0, var(--complex-box-shadow-default));
+}
+        
+li:not(.completed) {
+    background-color: var(--complex-background-color-6, var(--complex-background-color-default));}`);
+
+additionalStyleSheets[3] = new CSSStyleSheet();
+additionalStyleSheets[3].replaceSync(`li.completed {
+    background-color: var(--complex-background-color-3, var(--complex-background-color-default));
+}
+
+li:not(.completed):not([data-priority="4"]) {
+    border-bottom-color: var(--complex-border-bottom-color-1);
+}
+
+li.completed:not([data-priority="4"]) {
+    border-bottom-color: var(--complex-border-bottom-color-0);
+}
+
+li:not(.completed):not([data-priority="4"]) > div > label {
+    color: var(--complex-color-0, var(--complex-color-default));
+}
+
+li.completed:not([data-priority="4"]) > div > label {
+    color: var(--complex-color-1, var(--complex-color-default));
+}
+
+li.completed > div > :focus,
+li.completed > div > .toggle:focus + label {
+    box-shadow: var(--complex-box-shadow-0, var(--complex-box-shadow-default));
+}
+            
+li:not(.completed) {
+    background-color: var(--complex-background-color-7, var(--complex-background-color-default));}`);
 
 window.extraCssToAdopt = additionalStyleSheets;
