1bd5697f7c590ef500a35f5388b901ef121754ec
issack john <issackjohn@microsoft.com>
2023-08-11 11:48:33 -0700

    clean add-extra-css.js

--- resources/todomvc/vanilla-examples/javascript-web-components-complex/dist/add-extra-css.js
+++ resources/todomvc/vanilla-examples/javascript-web-components-complex/dist/add-extra-css.js
@@ -1,77 +1,33 @@
 const additionalStyleSheet = new CSSStyleSheet();
-// for loop this.
-additionalStyleSheet.replaceSync(`:host([data-priority="0"]) {
+for (let i = 0; i < 4; i++) {
+    additionalStyleSheet.insertRule(`:host([data-priority="${i}"]) {
     --complex-border-bottom-color-completed: var(--complex-border-bottom-color-0, var(--complex-border-bottom-color-default));
     --complex-border-bottom-color-not-completed: var(--complex-border-bottom-color-1, var(--complex-border-bottom-color-default));
 
-    --complex-background-color-completed: var(--complex-background-color-4, var(--complex-background-color-default));
-    --complex-background-color-not-completed: var(--complex-background-color-8, var(--complex-background-color-default));
-    
-    --complex-color-completed: var(--complex-color-1, var(--complex-color-default));
-    --complex-color-not-completed: var(--complex-color-0, var(--complex-color-default));
-
-    --complex-box-shadow: var(--complex-box-shadow-0, var(--complex-box-shadow-default));
-}
-
-:host([data-priority="1"]) {
-    --complex-border-bottom-color-completed: var(--complex-border-bottom-color-0, var(--complex-border-bottom-color-default));
-    --complex-border-bottom-color-not-completed: var(--complex-border-bottom-color-1, var(--complex-border-bottom-color-default));
-
-    --complex-background-color-completed: var(--complex-background-color-3, var(--complex-background-color-default));
-    --complex-background-color-not-completed: var(--complex-background-color-7, var(--complex-background-color-default));
+    --complex-background-color-completed: var(--complex-background-color-${4 - i}, var(--complex-background-color-default));
+    --complex-background-color-not-completed: var(--complex-background-color-${8 - i}, var(--complex-background-color-default));
 
     --complex-color-completed: var(--complex-color-1, var(--complex-color-default));
     --complex-color-not-completed: var(--complex-color-0, var(--complex-color-default));
 
     --complex-box-shadow: var(--complex-box-shadow-0, var(--complex-box-shadow-default));
+}`);
 }
-
-:host([data-priority="2"]) {
-    --complex-border-bottom-color-completed: var(--complex-border-bottom-color-0, var(--complex-border-bottom-color-default));
-    --complex-border-bottom-color-not-completed: var(--complex-border-bottom-color-1, var(--complex-border-bottom-color-default));
-
-    --complex-background-color-completed: var(--complex-background-color-2, var(--complex-background-color-default));
-    --complex-background-color-not-completed: var(--complex-background-color-6, var(--complex-background-color-default));
-
-    --complex-color-completed: var(--complex-color-1, var(--complex-color-default));
-    --complex-color-not-completed: var(--complex-color-0, var(--complex-color-default));
-
-    --complex-box-shadow: var(--complex-box-shadow-0, var(--complex-box-shadow-default));
-}
-
-:host([data-priority="3"]) {
-    --complex-border-bottom-color-completed: var(--complex-border-bottom-color-0, var(--complex-border-bottom-color-default));
-    --complex-border-bottom-color-not-completed: var(--complex-border-bottom-color-1, var(--complex-border-bottom-color-default));
-
-    --complex-background-color-completed: var(--complex-background-color-1, var(--complex-background-color-default));
-    --complex-background-color-not-completed: var(--complex-background-color-5, var(--complex-background-color-default));
-
-    --complex-color-completed: var(--complex-color-1, var(--complex-color-default));
-    --complex-color-not-completed: var(--complex-color-0, var(--complex-color-default));
-
-    --complex-box-shadow: var(--complex-box-shadow-0, var(--complex-box-shadow-default));
-}
-
-:host([completed="false"]) li {
+additionalStyleSheet.insertRule(`:host([completed="false"]) li {
     border-bottom-color: var(--complex-border-bottom-color-not-completed);
     background-color: var(--complex-background-color-not-completed);
-}
-
-:host([completed="true"]) li {
+}`);
+additionalStyleSheet.insertRule(`:host([completed="true"]) li {
     background-color: var(--complex-background-color-completed);
     border-bottom-color: var(--complex-border-bottom-color-completed);
-}
-
-:host([completed="false"]) li > div > label {
+}`);
+additionalStyleSheet.insertRule(`:host([completed="false"]) li > div > label {
     color: var(--complex-color-not-completed);
-}
-
-:host([completed="true"]) li > div > label {
+}`);
+additionalStyleSheet.insertRule(`:host([completed="true"]) li > div > label {
     color: var(--complex-color-completed);
-}
-
-:host([completed="true"]) li > div > :focus,
-:host([completed="true"]) li > div > .toggle:focus + label {
-    box-shadow: var(--complex-box-shadow);}`);
+}`);
+additionalStyleSheet.insertRule(`:host([completed="true"]) li > div > :focus,
+:host([completed="true"]) li > div > .toggle:focus + label { box-shadow: var(--complex-box-shadow);}`);
 
 window.extraCssToAdopt = additionalStyleSheet;
