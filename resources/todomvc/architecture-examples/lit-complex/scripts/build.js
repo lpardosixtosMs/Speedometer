dc4aa24c35e45b6823a1ad486bb6c9447a3477a8
issack john <issackjohn@microsoft.com>
2023-08-14 13:33:47 -0700

    rename the modules

--- resources/todomvc/architecture-examples/lit-complex/scripts/build.js
+++ resources/todomvc/architecture-examples/lit-complex/scripts/build.js
@@ -5,12 +5,12 @@ const SOURCE_DIRECTORY = "node_modules/todomvc-lit/dist/";
 const TITLE = "TodoMVC: Lit Complex DOM";
 const FILES_TO_MOVE = [
     "node_modules/big-dom-generator/dist/big-dom-generator.css",
-    "node_modules/big-dom-generator/utils/add-extra-css.js",
-    "node_modules/big-dom-generator/utils/todo-list-css.js",
+    "node_modules/big-dom-generator/utils/add-todo-item-extra-css.js",
+    "node_modules/big-dom-generator/utils/add-todo-list-extra-css.js",
     "node_modules/big-dom-generator/dist/logo.png",
     "app.css"
 ];
 const EXTRA_CSS_TO_LINK = ["app.css"];
-const SCRIPTS_TO_LINK = ["add-extra-css.js", "todo-list-css.js"];
+const SCRIPTS_TO_LINK = ["add-todo-item-extra-css.js", "add-todo-list-extra-css.js"];
 
 buildComplex(path.resolve(__dirname), path.join("..", SOURCE_DIRECTORY), TITLE, FILES_TO_MOVE, EXTRA_CSS_TO_LINK, SCRIPTS_TO_LINK);
