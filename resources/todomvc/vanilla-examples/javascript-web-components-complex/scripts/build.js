8a4d536f05c4a3ad15223eff2916dd2dc0f2bcce
issack john <issackjohn@microsoft.com>
2023-08-14 09:18:14 -0700

    additional stylesheet just to fix styling

--- resources/todomvc/vanilla-examples/javascript-web-components-complex/scripts/build.js
+++ resources/todomvc/vanilla-examples/javascript-web-components-complex/scripts/build.js
@@ -9,8 +9,9 @@ const FILES_TO_MOVE = [
     "node_modules/big-dom-generator/utils/javascript-web-components/add-extra-css.js",
     "node_modules/big-dom-generator/utils/todo-list-css.js",
     "node_modules/big-dom-generator/dist/logo.png",
+    "app.css"
 ];
-const EXTRA_CSS_TO_LINK = ["big-dom-generator-variables.css"];
+const EXTRA_CSS_TO_LINK = ["big-dom-generator-variables.css", "app.css"];
 const SCRIPTS_TO_LINK = ["add-extra-css.js", "todo-list-css.js"];
 
 buildComplex(path.resolve(__dirname), path.join("..", SOURCE_DIRECTORY), TITLE, FILES_TO_MOVE, EXTRA_CSS_TO_LINK, SCRIPTS_TO_LINK);
