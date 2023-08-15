69ebe224e07232fbab47a3d11f73395c35b7a2b2
issack john <issackjohn@microsoft.com>
2023-08-15 14:08:41 -0700

    remove test suite

--- resources/tests.mjs
+++ resources/tests.mjs
@@ -210,36 +210,6 @@ Suites.push({
 });
 
 Suites.push({
-    name: "TodoMVC-React-Complex-DOM-use-syntax",
-    url: "todomvc/architecture-examples/react-complex/dist-use-syntax/index.html#/home",
-    tags: ["todomvc", "complex"],
-    async prepare(page) {
-        const element = await page.waitForElement(".new-todo");
-        element.focus();
-    },
-    tests: [
-        new BenchmarkTestStep(`Adding${numberOfItemsToAdd}Items`, (page) => {
-            const newTodo = page.querySelector(".new-todo");
-            for (let i = 0; i < numberOfItemsToAdd; i++) {
-                newTodo.setValue(getTodoText(defaultLanguage, i));
-                newTodo.dispatchEvent("input");
-                newTodo.enter("keydown");
-            }
-        }),
-        new BenchmarkTestStep("CompletingAllItems", (page) => {
-            const checkboxes = page.querySelectorAll(".toggle");
-            for (let i = 0; i < numberOfItemsToAdd; i++)
-                checkboxes[i].click();
-        }),
-        new BenchmarkTestStep("DeletingAllItems", (page) => {
-            const deleteButtons = page.querySelectorAll(".destroy");
-            for (let i = numberOfItemsToAdd - 1; i >= 0; i--)
-                deleteButtons[i].click();
-        }),
-    ],
-});
-
-Suites.push({
     name: "TodoMVC-React-Redux",
     url: "todomvc/architecture-examples/react-redux/dist/index.html",
     tags: ["todomvc"],
