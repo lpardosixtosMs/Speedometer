accdd06990103f8271da23144c58494582636dfa
issack john <issackjohn@microsoft.com>
2023-08-15 13:57:52 -0700

    update build scripts

--- resources/todomvc/architecture-examples/react-complex/scripts/build.js
+++ resources/todomvc/architecture-examples/react-complex/scripts/build.js
@@ -5,8 +5,6 @@ const SOURCE_DIRECTORY = "node_modules/todomvc-react/dist/";
 const TITLE = "TodoMVC: React Complex DOM";
 const FILES_TO_MOVE = [
     "node_modules/big-dom-generator/dist/big-dom-generator.css",
-    "node_modules/big-dom-generator/dist/logo.png",
-    "node_modules/big-dom-generator/dist/Smock_TaskList_18_N.svg",
     "node_modules/big-dom-generator/utils/app.css",
 ];
 
