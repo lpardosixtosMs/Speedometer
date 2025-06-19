6959039a48c4fe512cd9818bb799054a1cd9911e
Luis Pardo <lpardosixtos@microsoft.com>
2025-06-19 23:15:18 +0000

    Removed indexeddb code

--- resources/todomvc/vanilla-examples/javascript-wc-indexeddb/src/components/todo-list/todo-list.template.js
+++
@@ -1,8 +0,0 @@
-const template = document.createElement("template");
-
-template.id = "todo-list-template";
-template.innerHTML = `
-    <ul class="todo-list display-none"></ul>
-`;
-
-export default template;
